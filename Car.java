76ddb1a33b20db9461640aeecd29caeadc8681f7
eatdostacos <64234158+eatdostacos@users.noreply.github.com>
2026-03-10 13:32:18 -0700

    Feat: changed car print

--- Car.java
+++ Car.java
@@ -10,6 +10,6 @@ public class Car {
     }
 
     public void printInfo() {
-        System.out.printf("This car is a %d %s %s%n", year, brand, model);
+        System.out.printf("Car: %d %s %s%n", year, brand, model);
     }
 }
